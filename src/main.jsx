56ab242bc9d23b1975116a962c2e4fdd84538396
Nina Schoellhammer <nschoel@pci128-13.etud.iut-orsay.fr>
2026-03-20 18:45:30 +0100

    Add files

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import NinaSchoellhammerProfile from './NinaSchoellhammerProfile'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NinaSchoellhammerProfile />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
